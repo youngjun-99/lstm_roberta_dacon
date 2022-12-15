46750d5f0240130ef91aa6bbe521f6998076a94f
노영준 <nyj5979@naver.com>
2022-12-15 21:54:01 +0900

    class_weight 추가
    효용성은 그다지... f1 스코어 하락하긴했음..

--- classifier.py
+++ classifier.py
@@ -6,10 +6,11 @@ from transformers.models.roberta.modeling_roberta import RobertaModel, RobertaPr
 from transformers.modeling_outputs import SequenceClassifierOutput
 
 class RobertaForSequenceClassification(RobertaPreTrainedModel):
-    def __init__(self, config):
+    def __init__(self, config, weights):
         super().__init__(config)
         self.num_labels = config.num_labels
         self.config = config
+        self.weights = weights
 
         self.roberta = RobertaModel(config)
         classifier_dropout = config.hidden_dropout_prob
@@ -67,7 +68,7 @@ class RobertaForSequenceClassification(RobertaPreTrainedModel):
                 if len(labels.size()) == len(logits.size()):
                     loss = softXEnt(logits.view(-1, self.num_labels), labels)
                 else:
-                    loss_fct = CrossEntropyLoss()
+                    loss_fct = CrossEntropyLoss(weight=self.weights)
                     loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
                 # print(loss)
 
