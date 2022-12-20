bffa6d6e9a2e6d53e22207ac70d7b5a0d4d11ae4
노영준 <nyj5979@naver.com>
2022-12-20 23:54:36 +0900

    arcface lossfunction with electra

--- custommodel.py
+++ custommodel.py
@@ -1,13 +1,13 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn import CrossEntropyLoss, MSELoss
+from torch.nn import CrossEntropyLoss, MSELoss, Parameter
 
 from transformers.models.roberta.modeling_roberta import RobertaModel, RobertaPreTrainedModel
 from transformers.modeling_outputs import SequenceClassifierOutput
 
-from loss import FocalLoss, softXEnt
-from transformers.models.electra.modeling_electra import ElectraModel, ElectraPreTrainedModel
+from loss import FocalLoss, softXEnt, AngularPenaltySMLoss
+from transformers.models.electra.modeling_electra import ElectraModel, ElectraPreTrainedModel, ElectraClassificationHead
 
 class RobertaForSequenceClassification(RobertaPreTrainedModel):
     def __init__(self, config):
@@ -16,7 +16,7 @@ class RobertaForSequenceClassification(RobertaPreTrainedModel):
         self.config = config
 
         self.roberta = RobertaModel(config)
-        classifier_dropout = config.hidden_dropout_prob
+        self.dropout_rate = config.hidden_dropout_prob
         self.lstm = nn.LSTM(
             input_size=config.hidden_size,
             hidden_size=config.hidden_size,
@@ -85,8 +85,79 @@ class RobertaForSequenceClassification(RobertaPreTrainedModel):
             hidden_states=outputs.hidden_states,
             attentions=outputs.attentions,
         )
-        
-        
-def softXEnt(input, target):
-    logprobs = torch.nn.functional.log_softmax(input, dim=1)
-    return -(target * logprobs).sum() / input.shape[0]
+
+
+class ElectraForSequenceClassification(ElectraPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.config = config
+        self.electra = ElectraModel(config)
+        self.classifier = ElectraClassificationHead(config)
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def forward(
+        self,
+        input_ids = None,
+        attention_mask = None,
+        token_type_ids = None,
+        position_ids = None,
+        head_mask = None,
+        inputs_embeds = None,
+        labels = None,
+        output_attentions = None,
+        output_hidden_states = None,
+        return_dict = None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        discriminator_hidden_states = self.electra(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        sequence_output = discriminator_hidden_states[0]
+        logits = self.classifier(sequence_output)
+
+        loss = None
+        if labels is not None:
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = AngularPenaltySMLoss(self.config.hidden_size, self.num_labels)
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = AngularPenaltySMLoss(self.config.hidden_size, self.num_labels)
+                loss = loss_fct(logits, labels)
+
+        if not return_dict:
+            output = (logits,) + discriminator_hidden_states[1:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=discriminator_hidden_states.hidden_states,
+            attentions=discriminator_hidden_states.attentions,
+        )
